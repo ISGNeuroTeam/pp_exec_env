24efb52e613ebf1ad762a11811b979614ee3add0
Alexander Lakeev <alakeev@gmail.com>
2022-02-09 18:57:58 +0300

    Added documentation and simple tests for CommandExecutor

--- pp_exec_env/command_executor.py
+++ pp_exec_env/command_executor.py
@@ -19,55 +19,92 @@ from pp_exec_env.sys_commands import (
 class CommandExecutor(eece.CommandExecutor):
     """
     Implementation of execution_environment.CommandExecutor.
+    This should be used by a Worker of Python Computing Node in order to execute PostProcessing commands.
 
     Attributes:
-        commands_directory: path to the folder with commands packages
         command_classes: a dictionary of command name and their classes
-        logger: logging.Logger instance
-        local_storage: path to mounted PostProcessing Local Storage
-        shared_storage: path to mounted PostProcessing Shared Storage
-        ips: path to mounted InterProcessing Storage
     """
 
+    logger = logging.getLogger("PostProcessing")
+
     def __init__(self, storages: dict[str, str], commands_directory: str):
-        self.logger = logging.getLogger("PostProcessing")
         self.logger.info("Initialization started")
-        self.local_storage = storages[LPP]
-        self.shared_storage = storages[SPP]
-        self.ips = storages[IPS]
+        self.logger.info("Importing system commands")
+        self.command_classes = self._import_sys_commands(local_storage=storages[LPP],
+                                                         shared_storage=storages[SPP],
+                                                         ips=storages[IPS])
 
-        self.command_classes: Dict[str, Type[BaseCommand]] = {}
-        self.commands_directory = commands_directory
+        self.logger.info("Importing user commands")
+        self.command_classes.update(self._import_user_commands(commands_directory))
 
-        self.logger.info("Importing commands")
-        self._import_sys_commands()
-        self._import_user_commands()
         self.logger.info("Initialization finished")
 
-    def _import_sys_commands(self):
+    @staticmethod
+    def _import_sys_commands(shared_storage: str, local_storage: str, ips: str) -> Dict[str, Type[BaseCommand]]:
         """
-        Initialize system commands in the CommandExecutor.
+        Import system commands and set paths to storages for each of them.
+
+        Args:
+            shared_storage: Path to PostProcessing Shared Storage
+            local_storage: Path to PostProcessing Local Storage
+            ips: Path to InterProcessing Storage
+        Returns:
+            A dictionary with command names as keys and command classes as values
+
+        Example Usage:
+
+        >>> from pp_exec_env.command_executor import CommandExecutor
+        >>> CommandExecutor._import_sys_commands("/tmp", "/tmp", "/tmp")
+        {'sys_read_interproc': <class '...'>, 'sys_write_interproc': <class '...'>, 'sys_write_result': <class '...'>}
         """
-        SysWriteResultCommand.shared_storage_path = self.shared_storage
-        SysWriteResultCommand.local_storage_path = self.local_storage
+        SysWriteResultCommand.shared_storage_path = shared_storage
+        SysWriteResultCommand.local_storage_path = local_storage
 
-        SysWriteResultCommand.ips_path = self.ips
-        SysReadInterProcCommand.ips_path = self.ips
-        SysWriteInterProcCommand.ips_path = self.ips
+        SysWriteResultCommand.ips_path = ips
+        SysReadInterProcCommand.ips_path = ips
+        SysWriteInterProcCommand.ips_path = ips
 
-        self.command_classes["sys_read_interproc"] = SysReadInterProcCommand
-        self.command_classes["sys_write_interproc"] = SysWriteInterProcCommand
+        command_classes = {
+            "sys_read_interproc": SysReadInterProcCommand,
+            "sys_write_interproc": SysWriteInterProcCommand,
+            "sys_write_result": SysWriteResultCommand
+        }
+        return command_classes
 
-        self.command_classes["sys_write_result"] = SysWriteResultCommand
-
-    def _import_user_commands(self):
+    @staticmethod
+    def _import_user_commands(commands_directory: str, follow_links: bool = True) -> Dict[str, Type[BaseCommand]]:
         """
-        Initialize custom commands in the CommandExecutor.
-        """
+        Import user-defined commands from the given folder.
+        The following rules are applied to the plugins:
 
-        for name in os.listdir(self.commands_directory):
-            path = os.path.join(self.commands_directory, name)
-            if os.path.isdir(path) and os.path.exists(init_path := os.path.join(path, '__init__.py')):
+        - Each command should be present in commands_directory as a separate folder.
+        - Command name is defined by its folder name.
+        - Each command should contain __init__.py in its root.
+        - __init__.py should define __all__.
+        - Command class should be the 1st element in the __all__ list.
+        - Plugin must support the same version of Python as the one in PostProcessing container image.
+        - Site-packages are expected to be installed in the image *for now*.
+
+        Args:
+            commands_directory: Path to the folder that contains plugins.
+            follow_links: If True, symbolic links will be treated as plugins as well.
+                          Can be useful for testing.
+        Returns:
+            A dictionary with command names as keys and command classes as values
+
+        Example Usage:
+
+        >>> from pp_exec_env.command_executor import CommandExecutor
+        >>> CommandExecutor._import_user_commands("../tests/resources/commands/", follow_links=False)
+        {'sum': <class 'plugin.sum.SumCommand'>, 'join': <class 'plugin.myjoin.JoinCommand'>}
+        """
+        command_classes = {}
+
+        for name in os.listdir(commands_directory):
+            path = os.path.join(commands_directory, name)
+
+            link_bool = (not os.path.islink(path)) or follow_links  # Either not a link or links are allowed
+            if os.path.isdir(path) and link_bool and os.path.exists(init_path := os.path.join(path, '__init__.py')):
                 spec = importlib.util.spec_from_file_location("plugin", init_path)
                 module = importlib.util.module_from_spec(spec)
                 sys.modules[spec.name] = module
@@ -75,26 +112,31 @@ class CommandExecutor(eece.CommandExecutor):
                 sys.modules.pop(spec.name)
 
                 if module.__dict__.get('__all__', None) is None or not module.__all__:  # Existence and emptiness
-                    self.logger.warning(f"Plugin {name} ignored, __all__ is empty or not found")
+                    CommandExecutor.logger.warning(f"Plugin {name} ignored, __all__ is empty or not found")
                     continue
 
                 cls_name = module.__all__[0]
                 cls = module.__getattribute__(cls_name)
                 if cls not in ['sys_read_interproc', 'sys_write_interproc', 'sys_write_result']:
-                    self.command_classes[name] = cls
+                    command_classes[name] = cls
+                    CommandExecutor.logger.info(f"Added command {name}")
                 else:
-                    self.logger.warning(f"Plugin {name} ignored, cannot redefine system command")
+                    CommandExecutor.logger.warning(f"Plugin {name} ignored, cannot redefine system command")
             else:
-                self.logger.warning(f"Plugin {name} ignored, either not a folder or no __init__.py")
+                CommandExecutor.logger.warning(f"Plugin {name} ignored, either not a folder or no __init__.py")
 
-    def execute(self, commands: List[Dict]):
+        return command_classes
+
+    def execute(self, commands: List[Dict]) -> pd.DataFrame:
         """
-        Execute OTLTree provided in `commands`
+        Execute a list of serialized OTL commands.
 
         Args:
-            commands: a list of all commands with their arguments to run
-        Raises:
-            Pretty much anything that and be raised
+            commands: List of dictionaries each containing serialized OTL commands.
+        Returns:
+            A pd.DataFrame that was created by given transformations.
+
+        For example usage consider looking at tests.
         """
         self.logger.info("Execution started")
         df = None
@@ -112,3 +154,8 @@ class CommandExecutor(eece.CommandExecutor):
             if not isinstance(df, pd.DataFrame):
                 raise ValueError("You're doing something spooky, command must return a DataFrame")
         return df
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(optionflags=doctest.ELLIPSIS)
